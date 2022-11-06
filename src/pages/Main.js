4619ec77a81ef386d697747f01ec3c31c8a5f725
Hepheir <hepheir@gmail.com>
2022-11-07 03:35:05 +0900

    feat(/): 목업 데이터 생성 코드 추가

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -14,6 +14,9 @@ export default function Main () {
         loadData();
     }, []);
 
+    // XXX: 디버그용 임시 목업 데이터 생성
+    setData(useMockData());
+
     return (
         <div>
             <h4>port 승인 테이블</h4>
@@ -29,19 +32,7 @@ export default function Main () {
                     </tr>
                 </thead>
                 <tbody>
-                    {data.map((item, index) => {
-                        const { data, port, request, service, mail, number } = item;
-                        return (
-                            <tr>
-                                <td>{data}</td>
-                                <td>{port}</td>
-                                <td>{request}</td>
-                                <td>{service}</td>
-                                <td>{mail}</td>
-                                <td>{number}</td>
-                            </tr>
-                        )
-                    })}
+                    {data.map((item) => createRow(item.data, item.port, item.request, item.service, item.mail, item.number))}
                 </tbody>
             </table>
             <Link to="/addport">
@@ -50,3 +41,37 @@ export default function Main () {
         </div>
     );
 };
+
+function useMockData() {
+    return [
+        {
+            data: '보민',
+            port: 3000,
+            request: 'request',
+            service: 'service',
+            mail: 'mail',
+            number: 'number',
+        },
+        {
+            data: '보민2',
+            port: 3001,
+            request: 'request2',
+            service: 'service2',
+            mail: 'mai2l',
+            number: 'numbe2r',
+        }
+    ]
+}
+
+function createRow(data, port, request, service, mail, number) {
+    return (
+        <tr>
+            <td>{data}</td>
+            <td>{port}</td>
+            <td>{request}</td>
+            <td>{service}</td>
+            <td>{mail}</td>
+            <td>{number}</td>
+        </tr>
+    );
+}
