6feb95714b3a574d2987c39520ee7a04d3b0de92
Hepheir <hepheir@gmail.com>
2022-11-07 05:13:53 +0900

    style(/Join): 코드 포멧터 적용

--- src/pages/Join.js
+++ src/pages/Join.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect} from "react";
-import {useNavigate, useParams, Link} from "react-router-dom";
+import React, { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
 import "react-toastify/dist/ReactToastify.css";
 import axios from "axios";
 
@@ -14,13 +14,13 @@ const initialState = {
 const AddEdit = () => {
 	const [state, setState] = useState(initialState);
 
-	const {name,userid,pw,mail,number} = state;
+	const { name, userid, pw, mail, number } = state;
 
 	const navigate = useNavigate();
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
-		if(!name || !userid || !pw || !mail || !number) {
+		if (!name || !userid || !pw || !mail || !number) {
 			alert("빈칸이 존재합니다.");
 		} else {
 			axios.post("http://localhost:3001/api/join", {
@@ -30,64 +30,64 @@ const AddEdit = () => {
 				mail,
 				number,
 			}).then(() => {
-				setState({name: "", userid: "", pw: "", mail: "", number: "" })
+				setState({ name: "", userid: "", pw: "", mail: "", number: "" })
 			})
-			setTimeout(() => { navigate("/");},2000);
+			setTimeout(() => { navigate("/"); }, 2000);
+		}
 	}
-	}
-	
+
 	const handleInputChange = (e) => {
-		const {name, value} = e.target;
-		setState({...state, [name]: value});
+		const { name, value } = e.target;
+		setState({ ...state, [name]: value });
 	};
-  return <div>
-	<form onSubmit={handleSubmit}>
-		<label htmlFor="name">Name</label>
-		<input
-			type="text"
-			id="name"
-			name="name"
-			value={name}
-			onChange={handleInputChange}
-		/>
-		<label htmlFor="userid">UserID</label>
-		<input
-			type="text"
-			id="userid"
-			name="userid"
-			value={userid}
-			onChange={handleInputChange}
-		/>
-		<label htmlFor='pw'>PW</label>
-		<input
-			type="password"
-			id="pw"
-			name="pw"
-			value={pw}
-			onChange={handleInputChange}
-		/>
-		<label htmlFor='email'>Email</label>
-		<input
-			type="text"
-			id="mail"
-			name="mail"
-			value={mail}
-			onChange={handleInputChange}
-		/>
-<label htmlFor='phone'>Phone</label>
-<input
-type="text"
-id="number"
-name="number"
-value={number}
-onChange={handleInputChange}
-/>
-<input type="submit" value="Sign up" />
-<Link to="/">
-	<input type="button" value="취소" />
-</Link>
-	</form>
-    </div>;
+	return <div>
+		<form onSubmit={handleSubmit}>
+			<label htmlFor="name">Name</label>
+			<input
+				type="text"
+				id="name"
+				name="name"
+				value={name}
+				onChange={handleInputChange}
+			/>
+			<label htmlFor="userid">UserID</label>
+			<input
+				type="text"
+				id="userid"
+				name="userid"
+				value={userid}
+				onChange={handleInputChange}
+			/>
+			<label htmlFor='pw'>PW</label>
+			<input
+				type="password"
+				id="pw"
+				name="pw"
+				value={pw}
+				onChange={handleInputChange}
+			/>
+			<label htmlFor='email'>Email</label>
+			<input
+				type="text"
+				id="mail"
+				name="mail"
+				value={mail}
+				onChange={handleInputChange}
+			/>
+			<label htmlFor='phone'>Phone</label>
+			<input
+				type="text"
+				id="number"
+				name="number"
+				value={number}
+				onChange={handleInputChange}
+			/>
+			<input type="submit" value="Sign up" />
+			<Link to="/">
+				<input type="button" value="취소" />
+			</Link>
+		</form>
+	</div>;
 };
 
 export default AddEdit;
