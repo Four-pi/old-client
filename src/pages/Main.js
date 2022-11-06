6a1c83e6735b724c13d290a89c73175e23a27ae8
Hepheir <hepheir@gmail.com>
2022-11-07 03:24:22 +0900

    style(/): format code

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from "react-router-dom";
 import axios from "axios";
 
@@ -6,46 +6,47 @@ const Main = () => {
     const [data, setData] = useState([]);
 
     const loadData = async () => {
-    const response = await axios.get("http://localhost:3001/api/port");
-    setData(response.data);
-};
+        const response = await axios.get("http://localhost:3001/api/port");
+        setData(response.data);
+    };
 
-useEffect(() => {
-    loadData();
-}, []);
-return(
-    <div>
-	<h4>port 승인 테이블</h4>
-        <table>
-            <thead>
-            <tr>
-                <th>date</th>
-                <th>port</th>
-                <th>request</th>
-                <th>service</th>
-                <th>mail</th>
-                <th>number</th>
-            </tr>
-            </thead>
-        <tbody>
-            {data.map((item, index) => {
-                return(
+    useEffect(() => {
+        loadData();
+    }, []);
+
+    return (
+        <div>
+            <h4>port 승인 테이블</h4>
+            <table>
+                <thead>
                     <tr>
-                        <td>{item.date}</td>
-                        <td>{item.port}</td>
-                        <td>{item.request}</td>
-                        <td>{item.service}</td>
-                        <td>{item.mail}</td>
-                        <td>{item.number}</td>
+                        <th>date</th>
+                        <th>port</th>
+                        <th>request</th>
+                        <th>service</th>
+                        <th>mail</th>
+                        <th>number</th>
                     </tr>
-                )
-            })}
-        </tbody>
-        </table>
-	<Link to="/addport">
-	<button>승인 신청</button>
-	</Link>
-    </div>
+                </thead>
+                <tbody>
+                    {data.map((item, index) => {
+                        return (
+                            <tr>
+                                <td>{item.date}</td>
+                                <td>{item.port}</td>
+                                <td>{item.request}</td>
+                                <td>{item.service}</td>
+                                <td>{item.mail}</td>
+                                <td>{item.number}</td>
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
+            <Link to="/addport">
+                <button>승인 신청</button>
+            </Link>
+        </div>
     );
 };
 
