b3a6cadf84edb25c6e460d4720949b346c3ecd72
Hepheir <hepheir@gmail.com>
2022-11-07 06:32:36 +0900

    test(/Reject): 목업 데이터 제거

--- src/pages/Reject.js
+++ src/pages/Reject.js
@@ -1,69 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from "axios";
 
-const useMockup = true;
-const mockupData = [
-    {
-        date: '보민',
-        port: 3000,
-        request: 'request',
-        service: 'service',
-        mail: 'mail',
-        number: 'number',
-    },
-    {
-        date: '보민2',
-        port: 3001,
-        request: 'request2',
-        service: 'service2',
-        mail: 'mai2l',
-        number: 'numbe2r',
-    },
-    {
-        date: "2022-11-031",
-        port: "1",
-        request: "1",
-        service: 1,
-        number: "1",
-        mail: 'mai2l',
-    },
-    {
-        date: "2022-11-031",
-        port: "1",
-        request: "1",
-        service: 1,
-        number: "1",
-    },
-    {
-        date: "2022-11-03a",
-        port: "a",
-        request: "a",
-        service: "a",
-        number: "a",
-    },
-    {
-        date: "2022-11-031",
-        port: "1",
-        request: "1",
-        service: 1,
-        number: "1",
-    },
-    {
-        date: "2022-11-033",
-        port: "3",
-        request: "3",
-        service: 3,
-        number: "3",
-    },
-    {
-        date: "2022-11-0312",
-        port: "asdasd",
-        request: "ssh",
-        service: "a@a.com",
-        number: "1111-1111-1111"
-    }
-];
-
 const Reject = () => {
     const [data, setData] = useState([]);
 
@@ -73,11 +10,6 @@ const Reject = () => {
     };
 
     useEffect(() => {
-        // XXX: 디버그용 임시 목업 데이터 생성
-        if (useMockup) {
-            setData(mockupData);
-        }
-
         loadData();
     }, []);
 
