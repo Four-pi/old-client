e86bd825e01ba59a5bd040d3548230209115686e
Hepheir <hepheir@gmail.com>
2022-11-07 04:38:39 +0900

    feat(/Reject): Reject 페이지 디자인 적용 (완)

--- src/pages/Reject.js
+++ src/pages/Reject.js
@@ -83,33 +83,37 @@ const Reject = () => {
 
     return (
         <div>
-            <h2>거절 테이블</h2>
-            <table>
-                <thead>
-                    <tr>
-                        <th>date</th>
-                        <th>port</th>
-                        <th>request</th>
-                        <th>service</th>
-                        <th>mail</th>
-                        <th>number</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {data.map((item, index) => {
-                        return (
-                            <tr>
-                                <td>{item.date}</td>
-                                <td>{item.port}</td>
-                                <td>{item.request}</td>
-                                <td>{item.service}</td>
-                                <td>{item.mail}</td>
-                                <td>{item.number}</td>
-                            </tr>
-                        )
-                    })}
-                </tbody>
-            </table>
+            <div class="my-2 p-2">
+                <h1>거절 테이블</h1>
+            </div>
+            <div class="my-2">
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th>date</th>
+                            <th>port</th>
+                            <th>request</th>
+                            <th>service</th>
+                            <th>mail</th>
+                            <th>number</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {data.map((item, index) => {
+                            return (
+                                <tr>
+                                    <td>{item.date}</td>
+                                    <td>{item.port}</td>
+                                    <td>{item.request}</td>
+                                    <td>{item.service}</td>
+                                    <td>{item.mail}</td>
+                                    <td>{item.number}</td>
+                                </tr>
+                            )
+                        })}
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
